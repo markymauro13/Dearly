c2b2e0cc750ddc727a29fcfe34607789f643caba
Mark Mauro <markymauro@gmail.com>
2025-10-27 14:57:59 -0400

    Initial Commit

---
+++ Dearly/DearlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  DearlyApp.swift
+//  Dearly
+//
+//  Created by Mark Mauro on 10/27/25.
+//
+
+import SwiftUI
+
+@main
+struct DearlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DearlyTests/DearlyTests.swift
@@ -0,0 +1,17 @@
+//
+//  DearlyTests.swift
+//  DearlyTests
+//
+//  Created by Mark Mauro on 10/27/25.
+//
+
+import Testing
+@testable import Dearly
+
+struct DearlyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
